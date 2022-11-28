ab9f938422963a3e5ed6847fc113509359ca30de
David93Code <david.armeni1993@gmail.com>
2022-11-28 13:48:19 +0200

    commit fix to form syntax

--- react_library_frontend/src/App.js
+++ react_library_frontend/src/App.js
@@ -1,10 +1,13 @@
 import Form from "./components/form";
+import Header from "./components/header";
 
 const App = () => {
 
   return (
     <div>
+      <Header />
       <Form />
+      <h2>Books</h2>
     </div>
   )
 
