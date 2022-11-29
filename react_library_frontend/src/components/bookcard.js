8c619bbc50862e5fd21e674640e23f698436d6ca
David93Code <david.armeni1993@gmail.com>
2022-11-29 09:37:54 +0200

    book card layout

--- react_library_frontend/src/components/bookcard.js
+++ react_library_frontend/src/components/bookcard.js
@@ -1,11 +1,12 @@
 
+
 const BookCard = () => {
 
     return (
         <div className="book--card">
-          <div className="conten">
-              <img/>
-              <div>
+          <div className="book--content">
+              <img className="book__image" src="./images/cover.jpg" />
+              <div className="book__info">
                   <h1>Title</h1>
                   <p>Description</p>
                   <p>Genre</p>
--- react_library_frontend/src/index.js
+++ react_library_frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './form.css';
+import './bookcard.css';
 import App from './App';
 
 
