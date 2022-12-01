a2dab633ba3d6ab7f6bc3b145aeeeb1642863cd2
hnnav <57915266+hnnav@users.noreply.github.com>
2022-12-01 10:23:38 +0200

    filter works but genres hardcoded

--- react_library_frontend/src/components/Search.js
+++ react_library_frontend/src/components/Search.js
@@ -13,15 +13,28 @@ function Search({ books }) {
         if (searchInput === '') {
             return book;
         } else {
-            return book.title.toLowerCase().includes(searchInput)
+            return book.title.toLowerCase().includes(searchInput) || book.genre.toLowerCase().includes(searchInput)
         } 
     })
 
     return (
         <div>
-            <div className="search-bar">
-                <ion-icon name="search-outline"></ion-icon>
-                <input onChange={searchHandler} name="search-input" placeholder="Search for a book..."></input>
+            <div className="search-and-filter">
+                <div className="search-bar">
+                    <ion-icon name="search-outline"></ion-icon>
+                    <input onChange={searchHandler} name="search-input" placeholder="Search for a book..."></input>
+                </div>
+
+                <div className="filter-by-genre">
+                    <select onChange={searchHandler} name="genres" id="genres">
+                        <option value="">Filter by Genre</option>
+                        <option value="horror">Horror</option>
+                        <option value="fantasy">Fantasy</option>
+                        <option value="nonfiction">Non-fiction</option>
+                        <option value="selfhelp">Self Help</option>
+                        <option value="fiction">Fiction</option>
+                    </select>
+                </div>
             </div>
             <BookCard filteredBooks={filteredBooks}/>
         </div>
