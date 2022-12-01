5530849f75e3e2fc9787af56f0816d9749ac308e
mk99995 <mejkorte@gmail.com>
2022-12-01 11:12:04 +0200

    added key with react fragment

--- react_library_frontend/src/components/bookcard.js
+++ react_library_frontend/src/components/bookcard.js
@@ -1,34 +1,42 @@
 import service from "../service/books";
+import React from "react";
 
+const BookCard = ({ filteredBooks }) => {
+  return (
+    <div className="book--content">
+      <div className="book__info">
+        {filteredBooks.map((book) => {
+          return (
+            <React.Fragment key={book.id}>
+              <div className="book--card">
+                <img
+                  className="delete__icon"
+                  src="./images/icon-close.svg"
+                  onClick={() => service.deleteBook(book.id)}
+                />
+                <img className="book__image" src="./images/cover.jpg" />
+                <h1>{book.title}</h1>
+                <p>Author: {book.author}</p>
+                <p>Genre: {book.genre}</p>
+                <p>{book.description}</p>
+                <p>{book.loanStatus ? "Loaned" : "Available"}</p>
 
-const BookCard = ( {filteredBooks} ) => {
-
-    return (
-        
-          <div className="book--content">
-              
-              <div className="book__info">
-                  {filteredBooks
-                  .map((book) => {
-                      return (
-                      <div className="book--card">
-                          <img className="delete__icon" src="./images/icon-close.svg" onClick={()=>service.deleteBook(book.id)} />
-                          <img className="book__image" src="./images/cover.jpg" />
-                      <h1>{book.title}</h1>
-                      <p>Author: {book.author}</p>
-                      <p>Genre: {book.genre}</p>
-                      <p>{book.description}</p>
-                      <p>{book.loanStatus ? 'Loaned' : 'Available'}</p>
-                      
-                      <button onClick={() => service.updateLoanStatus(book.id, {loanStatus: !book.loanStatus})}>{book.loanStatus ? 'Return' : 'Loan'}</button>
-                      </div>)
-                  })
+                <button
+                  onClick={() =>
+                    service.updateLoanStatus(book.id, {
+                      loanStatus: !book.loanStatus,
+                    })
                   }
-               
+                >
+                  {book.loanStatus ? "Return" : "Loan"}
+                </button>
               </div>
-          </div>
-       
-    )
-}
+            </React.Fragment>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default BookCard;
