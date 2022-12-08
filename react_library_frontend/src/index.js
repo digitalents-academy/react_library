ee25d3f2abc6f5f6666bf39b91252d4d0cbec97e
David93Code <david.armeni1993@gmail.com>
2022-12-08 10:22:17 +0200

    hover effect fixed

--- react_library_frontend/src/index.js
+++ react_library_frontend/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import './form.css';
-import './bookcard.css';
-import './search.css';
-import './app.css';
+
 import App from './App';
 
 
