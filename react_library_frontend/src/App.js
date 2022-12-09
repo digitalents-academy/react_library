dc6990d22e54eda2425e283902c507d13c120985
mk99995 <mejkorte@gmail.com>
2022-12-09 10:05:34 +0200

    login works

--- react_library_frontend/src/App.js
+++ react_library_frontend/src/App.js
@@ -2,37 +2,47 @@ import Form from "./components/form";
 import Header from "./components/header";
 import Search from "./components/Search";
 import service from "./service/books";
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
-import './index.css';
-import './form.css';
-import './bookcard.css';
-import './search.css';
-import './app.css';
-
+import "./index.css";
+import "./form.css";
+import "./bookcard.css";
+import "./search.css";
+import "./app.css";
 
 const App = () => {
-  
   const [books, setBooks] = useState([]);
-  
+  const [user, setUser] = useState("");
+
   useEffect(() => {
     service.getAll().then((book) => {
       setBooks(book);
     });
   }, [books]);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("user");
+    console.log("asd");
+    console.log(loggedUserJSON);
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      service.setToken(user.token);
+    }
+  }, []);
+
   return (
     <div>
       <Header />
       <Form />
-      <div style={{display: 'flex'}}>
-      <LoginForm />
-      <RegisterForm />
+      <div style={{ display: "flex" }}>
+        <LoginForm setUser={setUser} />
+        <RegisterForm />
       </div>
       <Search books={books} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- react_library_frontend/src/components/loginForm.js
+++ react_library_frontend/src/components/loginForm.js
@@ -1,27 +1,63 @@
+import { useState } from "react";
+import loginService from "../service/login";
+import service from "../service/books";
 
+const LoginForm = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-const LoginForm = () => {
+  const handleLogin = async (event) => {
+    event.preventDefault();
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-      console.log("daniele apua");
+    try {
+      const user = await loginService.login({
+        email,
+        password,
+      });
+      console.log(user);
+      window.localStorage.setItem("user", JSON.stringify(user));
+      service.setToken(user.token);
+      props.setUser(user);
+      setEmail("");
+      setPassword("");
+    } catch (exception) {
+      console.error(exception);
     }
+  };
 
-    return (
-        <div>
-            <form className="login--form" onSubmit={handleSubmit}>
-                <div className="email--input">
-                    <label htmlFor='email' className="email">email</label>
-                    <input type="email" name="email" id="email"></input>
-                </div>
-                <div className="password--input">
-                    <label htmlFor='password' className="password">password</label>
-                    <input type="password" name="password" id="password"></input>
-                </div>
-                <button type="submit" className="login--button">Login</button>
-            </form>
+  return (
+    <div>
+      <form className="login--form" onSubmit={handleLogin}>
+        <div className="email--input">
+          <label htmlFor="email" className="email">
+            email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+          ></input>
         </div>
-    )
-}
+        <div className="password--input">
+          <label htmlFor="password" className="password">
+            password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          ></input>
+        </div>
+        <button type="submit" className="login--button">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default LoginForm;
