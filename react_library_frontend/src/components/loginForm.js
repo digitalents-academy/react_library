ff40fd6f07ec33b4996cbfb03652bdfc040f4209
david armeni <david.armeni1993@gmail.com>
2022-12-15 22:50:35 +0200

    login/register window input style updated

--- react_library_frontend/src/components/loginForm.js
+++ react_library_frontend/src/components/loginForm.js
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import loginService from "../service/login";
 import service from "../service/books";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 import "../header.css";
 
 const LoginForm = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
- 
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -24,24 +23,33 @@ const LoginForm = (props) => {
       setEmail("");
       setPassword("");
       toast.success("Logged in");
-      props.setLoginWindow(false)
+      props.setLoginWindow(false);
     } catch (exception) {
       console.error(exception);
       toast.error("Incorrect email or password");
     }
-    
   };
+
+  // email.trim().length !== 0 ? className="notEmpty" : className="email--input input--container "
 
   return (
     <div className="loginFormContainer">
       <h2>Login</h2>
-      <form className="login--form" onSubmit={handleLogin} >
-        <div className="email--input input--container">
-          
+      <form className="login--form" onSubmit={handleLogin}>
+        <div className="email--input input--container ">
+          {/* <div
+          className={
+            email.trim().length !== 0
+              ? "notEmpty"
+              : "email--input input--container"
+          }
+        > */}
           <label htmlFor="email" className="email">
             Email:
           </label>
-          <input  
+          <input
+            // gives the class name if there is text in the input so the CSS can read the class and the background stays black when the input is not selected
+            className={email.trim().length !== 0 ? "notEmpty" : null}
             type="email"
             name="email"
             id="email"
@@ -54,22 +62,23 @@ const LoginForm = (props) => {
             Password:
           </label>
           <input
+            // gives the class name if there is text in the input so the CSS can read the class and the background stays black when the input is not selected
+            className={password.trim().length !== 0 ? "notEmpty" : null}
             type="password"
             name="password"
             id="password"
             value={password}
             onChange={({ target }) => setPassword(target.value)}
           ></input>
-         
         </div>
         <button type="submit" className="login--button submit--button">
           Login
         </button>
       </form>
 
-   
-        <p className="signUp signButton" onClick={() => props.loginOrRegister()}>sign up</p>
-
+      <p className="signUp signButton" onClick={() => props.loginOrRegister()}>
+        sign up
+      </p>
     </div>
   );
 };
